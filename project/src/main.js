57d4cadcbb5396ab53680a87237b003b0084c4eb
qq1318170063 <1318170063@qq.com>
2019-10-11 11:33:12 +0800

    rem

--- project/src/main.js
+++ project/src/main.js
@@ -7,6 +7,7 @@ import router from './router';
 import ElementUI from 'element-ui';
 // import store from './store';
 import './css/base.css';
+import 'amfe-flexible';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
