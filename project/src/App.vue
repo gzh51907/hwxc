a7f8f064198dd18992308fb90f6a0eb582593f40
qq1318170063 <1318170063@qq.com>
2019-10-11 14:34:23 +0800

    修改部分文件

--- project/src/App.vue
+++ project/src/App.vue
@@ -67,12 +67,13 @@ export default {
 // }
 .footer {
   width: 100%;
-  height: 60px;
-  background: #fff;
-  border-top: 0.013333rem solid #ccc;
+  height: 13.333vw;
+  padding-top: 1.333vw;
+  border-top: 1px solid #ccc;
   position: fixed;
+  z-index: 100;
   bottom: 0;
-  z-index: 2;
+  background: #fff;
   ul {
     width: 100%;
     height: 100%;
@@ -81,11 +82,16 @@ export default {
     list-style: none;
     justify-content: space-around;
     li {
+      height: 100%;
       width: 25%;
       display: flex;
       flex-direction: column;
+      justify-content: center;
       align-items: center;
-      font-size: 12px;
+      font-size: 3.467vw;
+      i{
+        font-size: 6vw;
+      }
       a {
         color: #555;
         text-decoration: none;
--- project/vue.config.js
+++ project/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
             postcss: {
                 plugins: [
                     require('postcss-pxtorem')({ // 把px单位换算成rem单位
-                        rootValue: 37.5, // 换算的基数(设计图750的根字体为32)
+                        rootValue: 32, // 换算的基数(设计图750的根字体为32)
                         selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
                         propList: ['*']
                     })
