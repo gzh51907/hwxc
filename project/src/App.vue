4f5434b9285f592c9557f39f87043ac988352ddd
huiyun <158722188@qq.com>
2019-10-11 13:58:50 +0800

    aa

--- project/src/App.vue
+++ project/src/App.vue
@@ -57,14 +57,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// #app {
-//   font-family: "Avenir", Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 .footer {
   width: 100%;
   height: 60px;
