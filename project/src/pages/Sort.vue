c6041654bb07a3fd47d8caa0a092fa58320dd0fc
qinhuiyun <52968642+qinhuiyun@users.noreply.github.com>
2019-10-13 13:02:10 +0800

    修改选项卡

--- project/src/pages/Sort.vue
+++ project/src/pages/Sort.vue
@@ -1,24 +1,24 @@
 <template>
     <div>分类
+        <Header></Header>
         <Footer></Footer>
     </div>
 
 </template>
 
 <script>
+import Header from "./Header.vue";
 import Footer from "./Footer.vue";
 
 export default {
-  data(){
-    return {
-
-    }
+  data() {
+    return {};
   },
-  components:{
-    Footer
-  }   
-}
+  components: {
+    Footer,
+    Header
+  }
+};
 </script>
 <style lang="scss">
-
 </style>
