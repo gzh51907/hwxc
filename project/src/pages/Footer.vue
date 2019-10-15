a42fd04455d6f19bd8d2b89f8128d4115280259a
qq1318170063 <1318170063@qq.com>
2019-10-15 21:18:59 +0800

    底部修改 21:18

--- project/src/pages/Footer.vue
+++ project/src/pages/Footer.vue
@@ -5,7 +5,7 @@
      >
         <router-link :to="item.path" >
           <i :class="item.icon" ></i>
-          <el-badge :value="cartlength" class="item" v-if="item.name==='cart'" style=" position: absolute;top:12px"><span>{{item.text}}</span></el-badge>
+          <el-badge :value="cartlength" class="item" v-if="item.name==='cart'" style="line-height:30px">{{item.text}}</el-badge>
           <template v-else>{{item.text}}</template>
         </router-link>
       </li>
@@ -52,7 +52,6 @@ export default {
     cartlength() {
       // return this.$store.state.cartlist.length;
       return this.$store.getters.cartlength;
-  
     },
     currentUser() {
       return this.$store.state.common.user;
@@ -81,7 +80,6 @@ export default {
 </script>
 
 <style lang="scss">
-
 .footer {
   width: 100%;
   height: 13.333vw;
@@ -92,7 +90,6 @@ export default {
   left: 0;
   bottom: 0;
   background-color: #fff;
-  position: absolute;
 
   ul {
     width: 100%;
@@ -109,15 +106,10 @@ export default {
       justify-content: center;
       align-items: center;
       font-size: 3.467vw;
-      span{
-        display: block;
-        margin-top: 56%;
-      }
       i {
         font-size: 6vw;
         color: #aaa;
       }
-      
       a {
         display: flex;
         flex-direction: column;
@@ -132,10 +124,6 @@ export default {
           color: #009e9f;
         }
       }
-    }
-    li:nth-child(3) i{
-        position: absolute;
-        top: 17%;
     }
   }
 }
