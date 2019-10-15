94af5eac3a988338e6756c51fabb84243965ea69
qq1318170063 <1318170063@qq.com>
2019-10-15 17:48:33 +0800

    17：48

--- project/src/pages/Cart.vue
+++ project/src/pages/Cart.vue
@@ -19,7 +19,7 @@
           <div class="GoodsCart-header-l">
             <el-checkbox class="checkbox-name">闪电送药</el-checkbox>
             <!-- <input type="checkbox" class="checkboxall">
-            闪电送药122-->
+            闪电送药-->
           </div>
           <div class="GoodsCart-header-r">免配送费, 最快25分钟送达</div>
         </div>
