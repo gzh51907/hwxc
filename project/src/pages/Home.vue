62464746a056a7480ae6f1202d1c3fe5dd5e628d
qq1318170063 <1318170063@qq.com>
2019-10-16 15:51:25 +0800

    15:51

--- project/src/pages/Home.vue
+++ project/src/pages/Home.vue
@@ -1,10 +1,3 @@
-<!--
- * @Description: In User Settings Edit
- * @Author: your name
- * @Date: 2019-10-12 17:00:43
- * @LastEditTime: 2019-10-12 17:00:43
- * @LastEditors: your name
- -->
 <template>
   <div>
     <Header></Header>
